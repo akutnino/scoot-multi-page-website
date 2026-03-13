ab49a9aa9a2c77ffd4cc0fc3adbad54612bd1cf5
Nino Akut <akutnino@gmail.com>
2026-03-14 03:27:05 +0800

    updated vite.config.ts file - updated vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,19 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+export default defineConfig(({ mode }) => {
+	const isProduction = mode === 'production';
+
+	return {
+		base: '/scoot-multi-page-website',
+		plugins: [
+			react(),
+			babel({
+				presets: [reactCompilerPreset()],
+			}),
+		],
+		build: {
+			sourcemap: !isProduction,
+		},
+	};
 });
