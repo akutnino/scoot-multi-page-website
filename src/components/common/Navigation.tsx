42872511c1a3cd3d865016d1c4b8e9b6bbc118da
Nino Akut <akutnino@gmail.com>
2026-03-19 01:29:59 +0800

    updated Navigation component - added Link components

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router';
+
 function Navigation() {
-	return <div>Navigation</div>;
+	return (
+		<div>
+			Navigation
+			<Link to={'/'}>Home</Link>
+			<Link to={'/about'}>About</Link>
+			<Link to={'/careers'}>Careers</Link>
+			<Link to={'/locations'}>Locations</Link>
+		</div>
+	);
 }
 
 export default Navigation;
