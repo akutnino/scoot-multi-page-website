3ec82435b20b8dcb672a6655246b03193ab12f81
Nino Akut <akutnino@gmail.com>
2026-03-22 19:49:55 +0800

    updated Navigation component - added semantic elements & classNames

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,12 +1,36 @@
 import { Link } from 'react-router';
 
+import LogoImage from './LogoImage.tsx';
+import CallToActionButton from './CallToActionButton.tsx';
+
 function Navigation() {
 	return (
-		<nav>
-			<Link to={'/'}>Home</Link>
-			<Link to={'/about'}>About</Link>
-			<Link to={'/careers'}>Careers</Link>
-			<Link to={'/locations'}>Locations</Link>
+		<nav className='navigation'>
+			<div className='navigation__wrapper'>
+				<div className='navigation__navListWrapper'>
+					<Link to={'/'}>
+						<LogoImage className='navigation__logoImage' />
+					</Link>
+
+					<ul className='navigation__navList'>
+						<li className='navigation__navItem'>
+							<Link to={'/about'}>About</Link>
+						</li>
+
+						<li className='navigation__navItem'>
+							<Link to={'/careers'}>Careers</Link>
+						</li>
+
+						<li className='navigation__navItem'>
+							<Link to={'/locations'}>Locations</Link>
+						</li>
+					</ul>
+				</div>
+
+				<CallToActionButton className='navigation__callToActionButton'>
+					Get Scootin
+				</CallToActionButton>
+			</div>
 		</nav>
 	);
 }
