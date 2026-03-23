cdc7d6aa92a1d2794e9dc77022a4839f484b372f
Nino Akut <akutnino@gmail.com>
2026-03-23 11:10:45 +0800

    updated AppLayout component - removed OutletWrapper component

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -5,14 +5,11 @@ import Navigation from '../common/Navigation.tsx';
 import CallToAction from '../common/CallToAction.tsx';
 import Footer from '../common/Footer.tsx';
 
-// IMPORTED FROM THE SAME FOLDER.
-import OutletWrapper from './OutletWrapper.tsx';
-
 function AppLayout({ children }: { children: ReactNode }) {
 	return (
 		<>
 			<Navigation />
-			<OutletWrapper>{children}</OutletWrapper>
+			<main>{children}</main>
 			<CallToAction />
 			<Footer />
 		</>
