d63fcb0830870d61cdfb01c21a769b97b0644291
Nino Akut <akutnino@gmail.com>
2026-03-26 11:09:02 +0800

    updated CareersHeader component - added semantic elements & classNames

--- src/components/contents/careers/CareersHeader.tsx
+++ src/components/contents/careers/CareersHeader.tsx
@@ -1,5 +1,26 @@
+import BackgroundImage from '../../common/BackgroundImage.tsx';
+import BackgroundPhoto from '../../common/BackgroundPhoto.tsx';
+
+import careers_locations_hero_desktop from '/src/assets/patterns/careers-locations-hero-desktop.jpg';
+import white_circles from '/src/assets/patterns/white-circles.svg';
+
 function CareersHeader() {
-	return <header>CareersHeader</header>;
+	return (
+		<header className='careersHeader'>
+			<h1 className='careersHeader__headline'>About</h1>
+
+			<BackgroundPhoto
+				className='careersHeader__backgroundPhoto'
+				src={careers_locations_hero_desktop}
+				alt='Top view of a road intersection'
+			/>
+
+			<BackgroundImage
+				className='careersHeader__whiteCirclesImage'
+				src={white_circles}
+			/>
+		</header>
+	);
 }
 
 export default CareersHeader;
