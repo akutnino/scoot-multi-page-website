4c09814971ee41156912ff558ab9988a97637431
Nino Akut <akutnino@gmail.com>
2026-03-27 23:17:07 +0800

    updated locations folder for components - added semantic elements & classNames

--- src/components/contents/locations/LocationsHeader.tsx
+++ src/components/contents/locations/LocationsHeader.tsx
@@ -1,5 +1,26 @@
+import BackgroundImage from '../../common/BackgroundImage.tsx';
+import BackgroundPhoto from '../../common/BackgroundPhoto.tsx';
+
+import careers_locations_hero_desktop from '/src/assets/images/careers-locations-hero-desktop.jpg';
+import white_circles from '/src/assets/patterns/white-circles.svg';
+
 function LocationsHeader() {
-	return <header>LocationsHeader</header>;
+	return (
+		<header>
+			<h1 className='locationsHeader__headline'>Locations</h1>
+
+			<BackgroundPhoto
+				className='locationsHeader__backgroundPhoto'
+				src={careers_locations_hero_desktop}
+				alt='Top view of a road intersection'
+			/>
+
+			<BackgroundImage
+				className='locationsHeader__whiteCirclesImage'
+				src={white_circles}
+			/>
+		</header>
+	);
 }
 
 export default LocationsHeader;
--- src/components/contents/locations/MapLocations.tsx
+++ src/components/contents/locations/MapLocations.tsx
@@ -1,5 +1,40 @@
+import BackgroundPhoto from '../../common/BackgroundPhoto.tsx';
+import CallToActionButton from '../../common/CallToActionButton.tsx';
+
+import world_map_desktop from '/src/assets/images/world-map-desktop.png';
+
 function MapLocations() {
-	return <section>MapLocations</section>;
+	return (
+		<section className='mapLocations'>
+			<div className='mapLocations__backgroundPhotoWrapper'>
+				<BackgroundPhoto
+					className='mapLocations__mapBackgroundPhoto'
+					src={world_map_desktop}
+					alt='world map outline photo'
+				/>
+
+				<div className='mapLocations__cityLocation'>New York</div>
+				<div className='mapLocations__cityLocation'>London</div>
+				<div className='mapLocations__cityLocation'>Yokoyama</div>
+				<div className='mapLocations__cityLocation'>Jakarta</div>
+			</div>
+
+			<div className='mapLocations__contentWrapper'>
+				<div className='mapLocations__descriptionWrapper'>
+					<h1 className='mapLocations__headline'>Your city not listed?</h1>
+					<p className='mapLocations__description'>
+						If you’d like to see Scoot in your hometown, be sure to let us know. We track
+						requests and plan launches based on demand. Feel free to message us by
+						clicking the link or messaging us on social.
+					</p>
+				</div>
+
+				<CallToActionButton className='mapLocations__messageUsButton'>
+					Message Us
+				</CallToActionButton>
+			</div>
+		</section>
+	);
 }
 
 export default MapLocations;
