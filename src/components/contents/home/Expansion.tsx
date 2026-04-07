b280c8fe9db33da06f2963d13f590ef0824f80be
Nino Akut <akutnino@gmail.com>
2026-04-08 04:10:12 +0800

    updated Expansion component - added element and className

--- src/components/contents/home/Expansion.tsx
+++ src/components/contents/home/Expansion.tsx
@@ -10,10 +10,9 @@ function Expansion() {
 	return (
 		<section className='expansion'>
 			<div className='expansion__backgroundImageWrapper'>
-				<BackgroundPhoto
-					className='expansion__nearYouImage'
-					src={near_you}
-					alt='Photo of buildings'
+				<BackgroundImage
+					className='expansion__circleImage'
+					src={circle}
 				/>
 
 				<BackgroundImage
@@ -21,25 +20,28 @@ function Expansion() {
 					src={right_arrow}
 				/>
 
-				<BackgroundImage
-					className='expansion__circleImage'
-					src={circle}
+				<BackgroundPhoto
+					className='expansion__nearYouImage'
+					src={near_you}
+					alt='Photo of buildings'
 				/>
 			</div>
 
-			<div className='expansion__headlineWrapper'>
-				<div className='expansion__contentWrapper'>
-					<h1 className='expansion__headline'>Coming to a city near you</h1>
-					<p className='expansion__description'>
-						Scoot is available in 4 major cities so far. We’re expanding rapidly, so be
-						sure to let us know if you want to see us in your hometown. We’re aiming to
-						let our scooters loose on 23 cities over the coming year.
-					</p>
-				</div>
+			<div className='expansion__contentWrapper'>
+				<div className='expansion__headlineWrapper'>
+					<div className='expansion__descriptionWrapper'>
+						<h1 className='expansion__headline'>Coming to a city near you</h1>
+						<p className='expansion__description'>
+							Scoot is available in 4 major cities so far. We’re expanding rapidly, so be
+							sure to let us know if you want to see us in your hometown. We’re aiming to
+							let our scooters loose on 23 cities over the coming year.
+						</p>
+					</div>
 
-				<CallToActionButton className='expansion__callToActionButton'>
-					Learn More
-				</CallToActionButton>
+					<CallToActionButton className='expansion__callToActionButton'>
+						Learn More
+					</CallToActionButton>
+				</div>
 			</div>
 		</section>
 	);
