c908a39aa99d918c0d709fcdc418aaba79731f50
Nino Akut <akutnino@gmail.com>
2026-04-08 04:48:42 +0800

    updated Payments component - added element and className

--- src/components/contents/home/Payments.tsx
+++ src/components/contents/home/Payments.tsx
@@ -9,19 +9,21 @@ import circle from '/src/assets/patterns/circle.svg';
 function Payments() {
 	return (
 		<section className='payments'>
-			<div className='payments__headlineWrapper'>
-				<div className='payments__contentWrapper'>
-					<h1 className='payments__headline'>Zero hassle payments</h1>
-					<p className='payments__description'>
-						Our payment is as easy as one two three. We accept most credit cards and debit
-						cards. You can also link your PayPal account inside the app. Need to pay
-						later? No worries! You can defer payment for up to a month.
-					</p>
-				</div>
+			<div className='payments__contentWrapper'>
+				<div className='payments__headlineWrapper'>
+					<div className='payments__descriptionWrapper'>
+						<h1 className='payments__headline'>Zero hassle payments</h1>
+						<p className='payments__description'>
+							Our payment is as easy as one two three. We accept most credit cards and
+							debit cards. You can also link your PayPal account inside the app. Need to
+							pay later? No worries! You can defer payment for up to a month.
+						</p>
+					</div>
 
-				<CallToActionButton className='payments__callToActionButton'>
-					Learn More
-				</CallToActionButton>
+					<CallToActionButton className='payments__callToActionButton'>
+						Learn More
+					</CallToActionButton>
+				</div>
 			</div>
 
 			<div className='payments__backgroundImageWrapper'>
