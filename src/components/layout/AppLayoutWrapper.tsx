0a3ca32cb4355eda66761154c7c18b7bcf83b5b9
Nino Akut <akutnino@gmail.com>
2026-05-20 00:59:03 +0800

    updated AppLayoutWrapper component - added MobileDrawer component

--- src/components/layout/AppLayoutWrapper.tsx
+++ src/components/layout/AppLayoutWrapper.tsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router';
 // IMPORTED FROM THE SAME FOLDER.
 import AppLayout from './AppLayout.tsx';
 import SuspenseWrapper from './SuspenseWrapper.tsx';
+import MobileDrawer from '../common/MobileDrawer.tsx';
 
 function AppLayoutWrapper() {
 	return (
 		<SuspenseWrapper>
 			<AppLayout>
+				<MobileDrawer />
 				<Outlet />
 			</AppLayout>
 		</SuspenseWrapper>
