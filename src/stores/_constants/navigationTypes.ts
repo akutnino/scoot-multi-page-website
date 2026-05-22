70dbb6bb5131a42dfc3fca9f300f10b92c5efacf
Nino Akut <akutnino@gmail.com>
2026-05-22 22:34:00 +0800

    created _constants folder for stores - added navigationTypes.ts

---
+++ src/stores/_constants/navigationTypes.ts
@@ -0,0 +1,7 @@
+export const navigationTypes: {
+	NAVIGATION_DIALOG_TOGGLE: 'navigation/dialogToggle';
+	NAVIGATION_ADD_REF: 'navigation/addRef';
+} = {
+	NAVIGATION_DIALOG_TOGGLE: 'navigation/dialogToggle',
+	NAVIGATION_ADD_REF: 'navigation/addRef',
+};
