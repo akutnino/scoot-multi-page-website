38c2af984e0709bdaab6fa9283e13b5032ed2090
Nino Akut <akutnino@gmail.com>
2026-05-26 00:56:18 +0800

    updated navigation-types.ts for actions - removed NavigationAddRefActionType

--- src/_types/stores/actions/navigation-types.ts
+++ src/_types/stores/actions/navigation-types.ts
@@ -1,15 +1,7 @@
-import type { RefObject } from 'react';
 import type { navigationTypes } from '../../../stores/_constants/navigationTypes.ts';
 
 export type NavigationDialogToggleActionType = {
 	type: typeof navigationTypes.NAVIGATION_DIALOG_TOGGLE;
 };
 
-export type NavigationAddRefActionType = {
-	type: typeof navigationTypes.NAVIGATION_ADD_REF;
-	payload: RefObject<HTMLDialogElement | null>;
-};
-
-export type NavigationReducerActionType =
-	| NavigationDialogToggleActionType
-	| NavigationAddRefActionType;
+export type NavigationReducerActionType = NavigationDialogToggleActionType;
