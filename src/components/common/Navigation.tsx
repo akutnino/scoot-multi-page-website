d02751a491ada362843dac77c9e4bfcf8da2b504
Nino Akut <akutnino@gmail.com>
2026-05-26 01:05:21 +0800

    updated Navigation component - added selector, dispatch, & handleDialogToggle

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,13 +1,50 @@
 import { Link } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectNavigation } from '../../stores/selectors/navigationSelectors.ts';
+import type { AppDispatch } from '../../_types/stores/types.ts';
+import type { NavigationInitialStateType } from '../../_types/stores/reducers/navigation-types.ts';
+import { navigationToggleDialog } from '../../stores/actions/navigationActions.ts';
 
 import LogoImage from './LogoImage.tsx';
 import CallToActionButton from './CallToActionButton.tsx';
+import BackgroundImage from './BackgroundImage.tsx';
+
+import hamburger from '/src/assets/icons/hamburger.svg';
+import close from '/src/assets/icons/close.svg';
 
 function Navigation() {
+	const dispatch: AppDispatch = useDispatch();
+
+	const { isIconToggled }: NavigationInitialStateType = useSelector(selectNavigation);
+
+	const handleDialogToggle = () => {
+		dispatch(navigationToggleDialog());
+	};
+
 	return (
 		<nav className='navigation '>
 			<div className='navigation__wrapper wrapper'>
 				<div className='navigation__navListWrapper'>
+					<button
+						className='navigation__openButton'
+						type='button'
+						title='button'
+						onClick={handleDialogToggle}
+						aria-label='open modal'
+					>
+						{isIconToggled ? (
+							<BackgroundImage
+								className='navigation__buttonImage--toggled'
+								src={close}
+							/>
+						) : (
+							<BackgroundImage
+								className='navigation__buttonImage--isToggled'
+								src={hamburger}
+							/>
+						)}
+					</button>
+
 					<Link
 						className='navigation__logoLink'
 						to={'/'}
