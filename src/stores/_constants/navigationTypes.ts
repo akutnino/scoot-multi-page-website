78dce1dae66537e8030de3621b0dd8c524c8509b
Nino Akut <akutnino@gmail.com>
2026-05-26 00:58:58 +0800

    updated navigationTypes.ts file - removed NAVIGATION_ADD_REF

--- src/stores/_constants/navigationTypes.ts
+++ src/stores/_constants/navigationTypes.ts
@@ -1,7 +1,5 @@
 export const navigationTypes: {
 	NAVIGATION_DIALOG_TOGGLE: 'navigation/dialogToggle';
-	NAVIGATION_ADD_REF: 'navigation/addRef';
 } = {
 	NAVIGATION_DIALOG_TOGGLE: 'navigation/dialogToggle',
-	NAVIGATION_ADD_REF: 'navigation/addRef',
 };
