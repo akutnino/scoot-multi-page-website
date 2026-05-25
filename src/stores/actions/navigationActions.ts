be4dddc4e70c01a8117575c6d2fca26b637ea902
Nino Akut <akutnino@gmail.com>
2026-05-26 01:00:01 +0800

    updated navigationActions.ts file - removed navigationAddRef

--- src/stores/actions/navigationActions.ts
+++ src/stores/actions/navigationActions.ts
@@ -1,19 +1,6 @@
-import type { RefObject } from 'react';
 import { navigationTypes } from '../_constants/navigationTypes.ts';
-import type {
-	NavigationAddRefActionType,
-	NavigationDialogToggleActionType,
-} from '../../_types/stores/actions/navigation-types.ts';
+import type { NavigationDialogToggleActionType } from '../../_types/stores/actions/navigation-types.ts';
 
 export function navigationToggleDialog(): NavigationDialogToggleActionType {
 	return { type: navigationTypes.NAVIGATION_DIALOG_TOGGLE };
-}
-
-export function navigationAddRef(
-	dialogRef: RefObject<HTMLDialogElement | null>
-): NavigationAddRefActionType {
-	return {
-		type: navigationTypes.NAVIGATION_ADD_REF,
-		payload: dialogRef,
-	};
 }
